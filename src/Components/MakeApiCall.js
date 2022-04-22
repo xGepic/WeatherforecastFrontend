fdc1292114185c30fcb65ca3bfd7116b7e9f3c6a
Steve <simanek.s@gmx.at>
2022-04-22 20:59:33 +0200

    Data Display works

--- src/Components/MakeApiCall.js
+++ src/Components/MakeApiCall.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Table from "react-bootstrap/Table";
 
 class MakeApiCall extends Component {
   constructor(props) {
     super(props);
-    this.state = { posts: [] };
+    this.state = { posts: [], loading: false };
   }
   componentDidMount() {
     const config = {
@@ -14,14 +16,45 @@ class MakeApiCall extends Component {
     axios
       .get(url, config)
       .then((response) => {
-        console.log(response);
+        this.setState({ posts: response.data, loading: true });
       })
       .catch((error) => {
         console.log(error);
       });
   }
+  static renderTable(myData) {
+    return (
+      <>
+        <h1>API Data</h1>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Temperature in C</th>
+              <th>Temperature in F</th>
+              <th>Summary</th>
+            </tr>
+          </thead>
+          <tbody>
+            {myData.map((forecasts) => (
+              <tr key={forecasts.date}>
+                <td>{forecasts.date}</td>
+                <td>{forecasts.temperatureC}</td>
+                <td>{forecasts.temperatureF}</td>
+                <td>{forecasts.summary}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </>
+    );
+  }
   render() {
-    return <h1>Weatherforecast Data</h1>;
+    return this.state.loading ? (
+      MakeApiCall.renderTable(this.state.posts)
+    ) : (
+      <p>Loading...</p>
+    );
   }
 }
 
